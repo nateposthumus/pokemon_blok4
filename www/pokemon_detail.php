c1f9381bd4441341b6be1e9364363f4f6c20b02d
nathanposthumus <80158058+nateposthumus@users.noreply.github.com>
2025-04-16 13:38:26 +0200

    filter 2.0 started creation pokemon create

--- www/pokemon_detail.php
+++ www/pokemon_detail.php
@@ -25,6 +25,7 @@ $pokemon = mysqli_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokemon details</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    
 </head>
 
 <body class="bg-gray-100 min-h-screen py-8">
